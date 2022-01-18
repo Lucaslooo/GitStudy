a2af5d2b7c0304fbf9ecd439bc0af5d933982262
Lucas <1257582010@qq.com>
2022-01-18 21:05:46 +0800

    second commit

--- git-test1/src/main/java/com/Lucaslooo/git/GitTest1.java
+++ git-test1/src/main/java/com/Lucaslooo/git/GitTest1.java
@@ -4,5 +4,6 @@ package com.Lucaslooo.git;
 public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("aaaaaaaa");
     }
 }
